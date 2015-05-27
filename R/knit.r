6ceb76345253f3eb34b2ed2760e1804ffe068c3d
indraniel <indraniel@gmail.com>
2015-05-26 19:19:27 -0500

    + use optparse for the knitr scripts

--- R/knit.r
+++ R/knit.r
@@ -1,11 +1,24 @@
 #!/usr/bin/env Rscript
 
-args <- commandArgs(trailingOnly=TRUE)
-file <- args[1]
-cat(paste("Processing: ", file, "\n"))
+library(optparse)
+
+opts.list <- list(
+    make_option(c("-o", "--outdir"), action="store", 
+                type="character", default="final",
+                help="The output directory to place the figures [default: 'final']"),
+
+    make_option(c("-i", "--input"), action="store", 
+                type="character", default=NA,
+                help="The input markdown file to process")
+)
+
+opts <- parse_args(OptionParser(option_list=opts.list))
+
+cat(paste("Input File: ", opts$input, "\n"))
+cat(paste("Output Figure Directory: ", opts$outdir, "\n"))
 
 library(knitr)
-opts_knit$set(base.dir = 'final') # base dir where to save figures
+opts_knit$set(base.dir = opts$outdir) # base dir where to save figures
 
-knit(file)
+knit(opts$input)
 #knit2html(file)
